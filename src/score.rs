7694c439b32bd6bf74f7f41cd9aa8632c4960ac7
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-16 16:21:45 +0530

    feat(score): return 0 if needle is empty

--- src/score.rs
+++ src/score.rs
@@ -1,3 +1,12 @@
-pub fn score(_haystack: &str, _needle: &str) -> u16 {
+pub fn score(_haystack: &str, needle: &str) -> u16 {
+    if needle.is_empty() {
+        return 0
+    }
     unimplemented!()
+}
+
+#[test]
+fn test_score() {
+    // Empty needle.
+    assert_eq!(score("πr²", ""), 0);
 }
