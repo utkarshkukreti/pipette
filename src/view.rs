a83ca32a156983040f7ef9e9356f8fa9aa0fad9c
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-17 09:25:36 +0530

    feat(View): add empty `update` function

--- src/view.rs
+++ src/view.rs
@@ -3,3 +3,9 @@ use std::io;
 pub struct View<W: io::Write> {
     writer: W
 }
+
+impl<W: io::Write> View<W> {
+    pub fn update(&mut self, _prompt: &str, _candidates: &[&str]) -> io::Result<()> {
+        Ok(())
+    }
+}
