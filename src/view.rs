f6ec99248c2a8803abbf80db0c26ea715748cb6e
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-17 09:30:35 +0530

    feat(View::update): hide the cursor at the start; show at the end

--- src/view.rs
+++ src/view.rs
@@ -6,6 +6,22 @@ pub struct View<W: io::Write> {
 
 impl<W: io::Write> View<W> {
     pub fn update(&mut self, _prompt: &str, _candidates: &[&str]) -> io::Result<()> {
-        Ok(())
+        // Hide the cursor.
+        try!(write!(self.writer, "\x1b[?25l"));
+
+        // Show the cursor.
+        write!(self.writer, "\x1b[?25h")
     }
+}
+
+#[test]
+fn test_view_update() {
+    let mut out = vec![];
+    {
+        let mut v = View {
+            writer: &mut out
+        };
+        v.update("f", &["foo", "bar", "baz", "quux"]).unwrap();
+    }
+    assert_eq!(out, b"\x1b[?25l\x1b[?25h");
 }
