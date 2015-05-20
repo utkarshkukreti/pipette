84ade85dd3b80de3e59b9e4a7909887181bfe23a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-20 19:51:14 +0530

    feat(Pipette): add `view` field

--- src/pipette.rs
+++ src/pipette.rs
@@ -1,9 +1,11 @@
 use std::io;
+use view::View;
 
 pub struct Pipette<R: io::Read, W: io::Write> {
     reader: R,
     writer: W,
-    candidates: Vec<String>
+    candidates: Vec<String>,
+    view: View
 }
 
 impl<R: io::Read, W: io::Write> Pipette<R, W> {
@@ -11,7 +13,8 @@ impl<R: io::Read, W: io::Write> Pipette<R, W> {
         Pipette {
             reader: reader,
             writer: writer,
-            candidates: candidates
+            candidates: candidates,
+            view: View::new()
         }
     }
 }
