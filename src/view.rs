a3592753c3cc4b60741efb2c024435a16a9851e0
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-20 19:49:37 +0530

    feat(View): don't store writer in a field

--- src/view.rs
+++ src/view.rs
@@ -1,37 +1,36 @@
 use std::io;
 
-pub struct View<W: io::Write> {
-    writer: W
-}
+pub struct View;
 
-impl<W: io::Write> View<W> {
-    pub fn new(writer: W) -> View<W> {
-        View {
-            writer: writer
-        }
+impl View {
+    pub fn new() -> View {
+        View
     }
 
-    pub fn update(&mut self, prompt: &str, candidates: &[&str]) -> io::Result<()> {
+    pub fn update<W: io::Write>(&mut self,
+                                writer: &mut W,
+                                prompt: &str,
+                                candidates: &[&str]) -> io::Result<()> {
         // Hide the cursor.
-        try!(write!(self.writer, "\x1b[?25l"));
+        try!(write!(writer, "\x1b[?25l"));
 
         // Clear the line and print the prompt.
-        try!(write!(self.writer, "\r\x1b[K> {}", prompt));
+        try!(write!(writer, "\r\x1b[K> {}", prompt));
 
         for candidate in candidates {
             // Go to the next line, clear the line, and print the candidate.
-            try!(write!(self.writer, "\n\r\x1b[K{}", candidate))
+            try!(write!(writer, "\n\r\x1b[K{}", candidate))
         }
 
         // Jump back to the last char of the prompt.
-        try!(write!(self.writer, "\x1b[{}A\x1b[{}G",
+        try!(write!(writer, "\x1b[{}A\x1b[{}G",
                     candidates.len(),
                     prompt.chars().count() + 3));
 
         // Show the cursor.
-        try!(write!(self.writer, "\x1b[?25h"));
+        try!(write!(writer, "\x1b[?25h"));
 
-        self.writer.flush()
+        writer.flush()
     }
 }
 
@@ -39,10 +38,8 @@ impl<W: io::Write> View<W> {
 fn test_view_update() {
     let mut out = vec![];
     {
-        let mut v = View {
-            writer: &mut out
-        };
-        v.update("f", &["foo", "bar", "baz", "quux"]).unwrap();
+        let mut v = View::new();
+        v.update(&mut out, "f", &["foo", "bar", "baz", "quux"]).unwrap();
     }
     assert_eq!(out, "!?25l\r!K> f
 \r!Kfoo
