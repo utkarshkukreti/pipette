11451d960ade5b8475f843abda97c048130c56b3
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-20 19:40:35 +0530

    feat(Pipette): add `new` function

--- src/pipette.rs
+++ src/pipette.rs
@@ -1,3 +1,11 @@
 pub struct Pipette {
     candidates: Vec<String>
 }
+
+impl Pipette {
+    pub fn new(candidates: Vec<String>) -> Pipette {
+        Pipette {
+            candidates: candidates
+        }
+    }
+}
