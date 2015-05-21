b425310d6ef10dc6e329a8f604d32d40fae578aa
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-21 08:06:58 +0530

    feat(View::update): highlight selected candidate

--- src/view.rs
+++ src/view.rs
@@ -10,16 +10,30 @@ impl View {
     pub fn update<W: io::Write>(&mut self,
                                 writer: &mut W,
                                 prompt: &str,
-                                candidates: &[&str]) -> io::Result<()> {
+                                candidates: &[&str],
+                                selected: usize) -> io::Result<()> {
         // Hide the cursor.
         try!(write!(writer, "\x1b[?25l"));
 
         // Clear the line and print the prompt.
         try!(write!(writer, "\r\x1b[K> {}", prompt));
 
-        for candidate in candidates {
-            // Go to the next line, clear the line, and print the candidate.
-            try!(write!(writer, "\n\r\x1b[K{}", candidate))
+        for (i, candidate) in candidates.iter().enumerate() {
+            // Go to the beginning of next line, and clear the line.
+            try!(write!(writer, "\n\r\x1b[K"));
+
+            if i == selected {
+                // Red background; White foreground.
+                try!(write!(writer, "\x1b[37;41m"))
+            }
+
+            // Print the candidate
+            try!(write!(writer, "{}", candidate));
+
+            if i == selected {
+                // Reset all SGR attributes.
+                try!(write!(writer, "\x1b[0m"))
+            }
         }
 
         // Jump back to the last char of the prompt.
@@ -39,11 +53,11 @@ fn test_view_update() {
     let mut out = vec![];
     {
         let mut v = View::new();
-        v.update(&mut out, "f", &["foo", "bar", "baz", "quux"]).unwrap();
+        v.update(&mut out, "f", &["foo", "bar", "baz", "quux"], 2).unwrap();
     }
     assert_eq!(out, "!?25l\r!K> f
 \r!Kfoo
 \r!Kbar
-\r!Kbaz
+\r!K!37;41mbaz!0m
 \r!Kquux!4A!4G!?25h".replace("!", "\x1b[").as_bytes());
 }
