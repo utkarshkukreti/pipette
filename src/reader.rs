e30378b237c40b3ee1d69c2a6455c3a628226c8c
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-26 11:38:09 +0530

    feat: add `Key` enum to `reader` module

--- src/reader.rs
+++ src/reader.rs
@@ -3,3 +3,16 @@ use std::io;
 pub struct Reader<R: io::Read> {
     inner: R
 }
+
+pub enum Key {
+    Char(char),
+    CtrlC,
+    CtrlH,
+    CtrlJ,
+    CtrlM,
+    CtrlN,
+    CtrlP,
+    CtrlU,
+    Delete,
+    Other
+}
