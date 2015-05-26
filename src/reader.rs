078ca38b93f0f4038eb555a509a2b09a83e6ae76
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-26 11:43:35 +0530

    feat(Reader): add `read` function for simple keys

--- src/reader.rs
+++ src/reader.rs
@@ -24,4 +24,40 @@ impl<R: io::Read> Reader<R> {
             inner: inner
         }
     }
+
+    pub fn read(&mut self) -> io::Result<Key> {
+        use self::Key::*;
+
+        let mut buf = [0];
+        try!(self.inner.read(&mut buf));
+        Ok(match buf[0] {
+            3 => CtrlC,
+            8 => CtrlH,
+            10 => CtrlJ,
+            13 => CtrlM,
+            14 => CtrlN,
+            16 => CtrlP,
+            21 => CtrlU,
+            127 => Delete,
+            _ => Other
+        })
+    }
+}
+
+#[test]
+fn test_reader_read() {
+    use self::Key::*;
+
+    let input = [3, 8, 10, 13, 14, 16, 21, 127, 1];
+    let mut reader = Reader::new(&input[..]);
+
+    assert_eq!(reader.read().unwrap(), CtrlC);
+    assert_eq!(reader.read().unwrap(), CtrlH);
+    assert_eq!(reader.read().unwrap(), CtrlJ);
+    assert_eq!(reader.read().unwrap(), CtrlM);
+    assert_eq!(reader.read().unwrap(), CtrlN);
+    assert_eq!(reader.read().unwrap(), CtrlP);
+    assert_eq!(reader.read().unwrap(), CtrlU);
+    assert_eq!(reader.read().unwrap(), Delete);
+    assert_eq!(reader.read().unwrap(), Other);
 }
