52aa8d3e4301933dcc19002a1609d952bfd096d4
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-06-20 12:17:01 +0530

    feat(Pipette::exec): handle `CtrlP`

--- src/pipette.rs
+++ src/pipette.rs
@@ -33,6 +33,11 @@ impl<R: io::Read, W: io::Write> Pipette<R, W> {
                         if selected + 1 < self.candidates.len() {
                             selected += 1
                         }
+                    },
+                    CtrlP => {
+                        if selected > 0 {
+                            selected -= 1;
+                        }
                     }
                     _ => unimplemented!()
                 },
@@ -44,12 +49,12 @@ impl<R: io::Read, W: io::Write> Pipette<R, W> {
 
 #[test]
 fn test_pipette_exec() {
-    let input = b"\x0e\x0e\x0e\x0e\x0d";
+    let input = b"\x0e\x0e\x0e\x0e\x10\x10\x10\x10\x0d";
     let mut out = vec![];
     let candidates = vec!["foo".into(), "bar".into(), "baz".into()];
     {
         let mut pipette = Pipette::new(&input[..], &mut out, candidates);
-        assert_eq!(pipette.exec(), Some("baz".into()));
+        assert_eq!(pipette.exec(), Some("foo".into()));
     }
     assert_eq!(out, "\
 !?25l\r!K>\x20
@@ -72,5 +77,21 @@ fn test_pipette_exec() {
 \r!Kfoo
 \r!Kbar
 \r!K!37;41mbaz!0m!3A!3G!?25h\
+!?25l\r!K>\x20
+\r!Kfoo
+\r!K!37;41mbar!0m
+\r!Kbaz!3A!3G!?25h\
+!?25l\r!K>\x20
+\r!K!37;41mfoo!0m
+\r!Kbar
+\r!Kbaz!3A!3G!?25h\
+!?25l\r!K>\x20
+\r!K!37;41mfoo!0m
+\r!Kbar
+\r!Kbaz!3A!3G!?25h\
+!?25l\r!K>\x20
+\r!K!37;41mfoo!0m
+\r!Kbar
+\r!Kbaz!3A!3G!?25h\
 ".replace("!", "\x1b[").as_bytes());
 }
